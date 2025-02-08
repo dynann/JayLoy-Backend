eea6536501a3481e20838f5b13d2144f6c1dec71
dynann <dynannran@gmail.com>
2025-02-08 23:35:07 +0700

    Add user management module with CRUD operations and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //api swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Aha Smart')
+    .setDescription(
+      'this is aha smart api, you can do database manipulation through this ui',
+    )
+    .setVersion('1.0')
+    .addTag('aha smart')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    customfavIcon:
+      'https://res-console.cloudinary.com/dlbbfck9n/media_explorer_thumbnails/e21d9307936995dc631dc520ee985775/detailed',
+    customSiteTitle: 'AhaSmartAPI',
+    customCss: '.topbar { display: none; }',
+  });
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`app run on http://localhost:3000`);
 }
 bootstrap();
