7f012f003ffa81a689350e2d044d6b28a1754e86
dynann <dynannran@gmail.com>
2025-03-08 06:45:51 +0700

    feat: budget service

--- src/budgets/budgets.controller.ts
+++ src/budgets/budgets.controller.ts
@@ -1,34 +1,76 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, Query } from '@nestjs/common';
 import { BudgetsService } from './budgets.service';
-import { CreateBudgetDto } from './dto/create-budget.dto';
+import { CreateBudgetDto, GetBudgetDto } from './dto/create-budget.dto';
 import { UpdateBudgetDto } from './dto/update-budget.dto';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiProperty, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/config/contants';
 
+@ApiTags('Budgets')
+@ApiBearerAuth()
 @Controller('budgets')
 export class BudgetsController {
   constructor(private readonly budgetsService: BudgetsService) {}
 
   @Post()
-  create(@Body() createBudgetDto: CreateBudgetDto) {
-    return this.budgetsService.create(createBudgetDto);
+  @ApiOperation({ summary: 'create one budget' })
+  @ApiBody({ type: CreateBudgetDto })
+  @ApiResponse({ status: 200, type: GetBudgetDto })
+  async create(@Request() req:any, @Body() createBudgetDto: CreateBudgetDto) {
+    return this.budgetsService.createBudget(req.user.sub, createBudgetDto);
   }
 
+  @Public()
   @Get()
-  findAll() {
-    return this.budgetsService.findAll();
+  @ApiOperation({ summary: 'get all budgets' })
+  @ApiResponse({ status: 200, type: [GetBudgetDto] })
+  @ApiQuery({ name: 'userID', required: false, type: Number })
+  @ApiQuery({ name: 'startDate', required: false, type: String, example: '2025-03-07T00:00:00.000Z' })
+  @ApiQuery({ name: 'endDate', required: false, type: String, example: '2025-03-07T23:59:59.000Z' })
+  async findAll(@Query('userID') userID: number,
+                @Query('startDate') startDate: string,
+                @Query('endDate') endDate: string) {
+              
+    const where: any = {}
+    if(userID){
+      where.userID = +userID
+    }
+    if(startDate){
+      where.startDate = { gte: startDate }
+    }
+    if(endDate){
+      where.endDate = { lte: endDate }
+    }
+    return this.budgetsService.findAllBudget({
+      where: where
+    });
+  }
+
+  @Get('/mine')
+  @ApiOperation({ summary: 'get all my budget' })
+  @ApiResponse({ status: 200, type: [GetBudgetDto] })
+  async getAllMyBudget(@Request() req: any) {
+    return this.budgetsService.findAllBudget(req.user.sub)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.budgetsService.findOne(+id);
+  @ApiOperation({ summary: 'get one buget' })
+  @ApiResponse({ status: 200, type: GetBudgetDto })
+  async findOne(@Request() req: any ,@Param('id') id: string) {
+    return this.budgetsService.findOneBudget(req.user.sub, +id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBudgetDto: UpdateBudgetDto) {
-    return this.budgetsService.update(+id, updateBudgetDto);
+  @ApiOperation({ summary: 'update one budget' })
+  @ApiBody({ type: UpdateBudgetDto })
+  @ApiResponse({ status: 200, type: GetBudgetDto })
+  update(@Request() req: any, @Param('id') id: string, @Body() updateBudgetDto: UpdateBudgetDto) {
+    return this.budgetsService.updateOneBudget(req.user.sub ,+id, updateBudgetDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.budgetsService.remove(+id);
+  @ApiOperation({ summary: 'delete one budget' })
+  @ApiResponse({ status: 200, type: GetBudgetDto })
+  remove(@Request() req: any, @Param('id') id: string) {
+    return this.budgetsService.removeOneBudget(req.user.sub, +id);
   }
 }
--- src/budgets/dto/create-budget.dto.ts
+++ src/budgets/dto/create-budget.dto.ts
@@ -1,1 +1,50 @@
-export class CreateBudgetDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import * as dayjs from "dayjs"
+
+export class CreateBudgetDto {
+    @ApiProperty({ example: 1000 })
+    amount: number;
+
+    @ApiProperty({ example: dayjs() })
+    startDate: string;
+
+    @ApiProperty({ example: dayjs() })
+    endDate: string;
+}
+
+export class GetBudgetDto {
+
+    @ApiProperty({ example: 1 })
+    id: number;
+
+    @ApiProperty({ example: 1000 })
+    amount: number;
+
+    @ApiProperty({ example: dayjs() })
+    startDate: string;
+
+    @ApiProperty({ example: dayjs() })
+    endDate: string;
+
+    @ApiProperty({ example: 1 })
+    user: number;
+    constructor(budget: any){
+        this.id = budget.id
+        this.amount = Number(budget.amount)
+        this.startDate = budget.startDate
+        this.endDate = budget.endDate
+        this.user = budget.userID
+    }
+}
+
+export class UpdateBudgetDto {
+    @ApiProperty({ example: 1000 })
+    amount?: number;
+
+    @ApiProperty({ example: dayjs() })
+    startDate?: string;
+
+    @ApiProperty({ example: dayjs() })
+    endDate?: string;
+}
+
